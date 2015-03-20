4447a84821f646fbab923ebb48073730cbf234f5
Sam Whited <sam@samwhited.com>
2015-03-20 11:53:07 -0500

    Quick fix for location providers not existing

--- src/main/java/com/samwhited/opensharelocationplugin/activities/LocationActivity.java
+++ src/main/java/com/samwhited/opensharelocationplugin/activities/LocationActivity.java
@@ -13,32 +13,38 @@ public abstract class LocationActivity extends Activity implements LocationListe
 	private LocationManager locationManager;
 
 	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-	}
+		protected void onCreate(final Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		}
 
 	protected abstract void gotoLoc() throws UnsupportedOperationException;
 	protected abstract void setLoc(final Location location);
 
 	protected void requestLocationUpdates() {
-		final Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		if (lastKnownLocation != null) {
-			setLoc(lastKnownLocation);
-			try {
-				gotoLoc();
-			} catch (final UnsupportedOperationException ignored) {
+		if (locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER)) {
+			final Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			if (lastKnownLocation != null) {
+				setLoc(lastKnownLocation);
+				try {
+					gotoLoc();
+				} catch (final UnsupportedOperationException ignored) {
+				}
 			}
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, Config.LOCATION_FIX_TIME_DELTA,
+					Config.LOCATION_FIX_SPACE_DELTA, this);
 		}
 
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, Config.LOCATION_FIX_TIME_DELTA,
-				Config.LOCATION_FIX_SPACE_DELTA, this);
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, Config.LOCATION_FIX_TIME_DELTA,
-				Config.LOCATION_FIX_SPACE_DELTA, this);
+		if (locationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER)) {
+			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, Config.LOCATION_FIX_TIME_DELTA,
+					Config.LOCATION_FIX_SPACE_DELTA, this);
+		}
 
 		// If something else is also querying for location more frequently than we are, the battery is already being
 		// drained. Go ahead and use the existing locations as often as we can get them.
-		locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, this);
+		if (locationManager.getAllProviders().contains(LocationManager.PASSIVE_PROVIDER)) {
+			locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, this);
+		}
 	}
 
 	protected void pauseLocationUpdates() {
@@ -46,16 +52,16 @@ public abstract class LocationActivity extends Activity implements LocationListe
 	}
 
 	@Override
-	protected void onPause() {
-		super.onPause();
-		pauseLocationUpdates();
-	}
+		protected void onPause() {
+			super.onPause();
+			pauseLocationUpdates();
+		}
 
 	@Override
-	protected void onResume() {
-		super.onResume();
-		this.setLoc(null);
+		protected void onResume() {
+			super.onResume();
+			this.setLoc(null);
 
-		requestLocationUpdates();
-	}
+			requestLocationUpdates();
+		}
 }
